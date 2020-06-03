fcc0e92a7b123b3a6cec6ffa355bed51740ec9cd
Scott  Keplinger <skeplin17@gmail.com>
2020-06-03 23:00:08 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
--- lib/insert.sql
+++ lib/insert.sql
@@ -1,34 +1,37 @@
+
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+INSERT INTO projects VALUES ("The Big One", "Technology", 10000, "3/10/20", "12/10/20");
+
 INSERT INTO users (id, name, age) VALUES (1, "Scott", 32);
-INSERT INTO users VALUES ("Steve", 21);
-INSERT INTO users VALUES ("Abby", 44);
-INSERT INTO users VALUES ("Urkel", 51);
-INSERT INTO users VALUES ("Howie", 23);
-INSERT INTO users VALUES ("Eddy", 35);
-INSERT INTO users VALUES ("Ignacio", 62);
-INSERT INTO users VALUES ("Polly", 52);
-INSERT INTO users VALUES ("Jess", 22);
-INSERT INTO users VALUES ("Greg", 36);
-INSERT INTO users VALUES ("Wanda", 33);
-INSERT INTO users VALUES ("Natalie", 33);
-INSERT INTO users VALUES ("Kent", 27);
-INSERT INTO users VALUES ("Max", 30);
-INSERT INTO users VALUES ("PJ", 21);
-INSERT INTO users VALUES ("Scott", 32);
-INSERT INTO users VALUES ("Luke", 39);
-INSERT INTO users VALUES ("Vick", 45);
-INSERT INTO users VALUES ("Barry", 32);
-INSERT INTO users VALUES ("Dean", 42);
-
-
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
 
 INSERT INTO pledges VALUES ()
+
+
+
+-- INSERT INTO users VALUES ("Steve", 21);
+-- INSERT INTO users VALUES ("Abby", 44);
+-- INSERT INTO users VALUES ("Urkel", 51);
+-- INSERT INTO users VALUES ("Howie", 23);
+-- INSERT INTO users VALUES ("Eddy", 35);
+-- INSERT INTO users VALUES ("Ignacio", 62);
+-- INSERT INTO users VALUES ("Polly", 52);
+-- INSERT INTO users VALUES ("Jess", 22);
+-- INSERT INTO users VALUES ("Greg", 36);
+-- INSERT INTO users VALUES ("Wanda", 33);
+-- INSERT INTO users VALUES ("Natalie", 33);
+-- INSERT INTO users VALUES ("Kent", 27);
+-- INSERT INTO users VALUES ("Max", 30);
+-- INSERT INTO users VALUES ("PJ", 21);
+-- INSERT INTO users VALUES ("Scott", 32);
+-- INSERT INTO users VALUES ("Luke", 39);
+-- INSERT INTO users VALUES ("Vick", 45);
+-- INSERT INTO users VALUES ("Barry", 32);
+-- INSERT INTO users VALUES ("Dean", 42);
