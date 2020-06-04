8450ddb187e39f184ac0baefe16aea2a170d5576
scottvonscott <skeplin17@gmail.com>
2020-06-03 18:28:28 -0700

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,34 +1,67 @@
-INSERT INTO users (id, name, age) VALUES (1, "Scott", 32);
-INSERT INTO users VALUES ("Steve", 21);
-INSERT INTO users VALUES ("Abby", 44);
-INSERT INTO users VALUES ("Urkel", 51);
-INSERT INTO users VALUES ("Howie", 23);
-INSERT INTO users VALUES ("Eddy", 35);
-INSERT INTO users VALUES ("Ignacio", 62);
-INSERT INTO users VALUES ("Polly", 52);
-INSERT INTO users VALUES ("Jess", 22);
-INSERT INTO users VALUES ("Greg", 36);
-INSERT INTO users VALUES ("Wanda", 33);
-INSERT INTO users VALUES ("Natalie", 33);
-INSERT INTO users VALUES ("Kent", 27);
-INSERT INTO users VALUES ("Max", 30);
-INSERT INTO users VALUES ("PJ", 21);
-INSERT INTO users VALUES ("Scott", 32);
-INSERT INTO users VALUES ("Luke", 39);
-INSERT INTO users VALUES ("Vick", 45);
-INSERT INTO users VALUES ("Barry", 32);
-INSERT INTO users VALUES ("Dean", 42);
+INSERT INTO users (id, name, age) VALUES (1, "Scott", 32),
+(2, "Scott", 32),
+ (3, "Scott", 32),
+ (4, "Scott", 32),
+ (5, "Scott", 32),
+ (6, "Scott", 32),
+ (7, "Scott", 32),
+ (8, "Scott", 32),
+ (9, "Scott", 32),
+ (10, "Scott", 32),
+ (11, "Scott", 32),
+ (12, "Scott", 32),
+ (13, "Scott", 32),
+ (14, "Scott", 32),
+ (15, "Scott", 32),
+ (16, "Scott", 32),
+ (17, "Scott", 32),
+ (18, "Scott", 32),
+ (19, "Scott", 32),
+ (20, "Scott", 32);
 
 
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
-INSERT INTO projects VALUES ("The Big One", "Technology", 10000, 3/10/20, 12/10/20);
 
-INSERT INTO pledges VALUES ()
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(2, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(3, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(4, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(5, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(6, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(7, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(8, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(9, "The Big One", "Technology", 10000, 3/10/20, 12/10/20),
+(10, "The Big One", "Technology", 10000, 3/10/20, 12/10/20);
+
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
